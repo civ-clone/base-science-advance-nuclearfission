b54f2dac90d65c0bce3caaf5c4c12254155803d4
dom111 <dom111@users.noreply.github.com>
2022-02-25 20:37:34 +0000

    Initial commit.

---
+++ NuclearFission.d.ts
@@ -0,0 +1,3 @@
+import Advance from '@civ-clone/core-science/Advance';
+export declare class NuclearFission extends Advance {}
+export default NuclearFission;
---
+++ NuclearFission.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.NuclearFission = void 0;
+const Advance_1 = require("@civ-clone/core-science/Advance");
+class NuclearFission extends Advance_1.default {
+}
+exports.NuclearFission = NuclearFission;
+exports.default = NuclearFission;
+//# sourceMappingURL=NuclearFission.js.map
